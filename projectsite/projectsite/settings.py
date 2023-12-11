8e8386087df3a29f18bd8ef0638d2e9e3cb6aae9
VinceBorgyEspina <202180167@psu.palawan.edu.ph>
2023-12-11 19:42:14 +0800

    allowed_host

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t=e_+&8oahriluk0n#d8-i&x=bsv!sik(tyta537c-%ug4uy(_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['BorgyVince.pythonanywhere.com']
 
 
 # Application definition
